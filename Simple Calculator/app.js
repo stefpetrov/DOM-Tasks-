225c731bbecb3094f4582de0125095b4d665a3c2
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-06-03 11:37:35 +0300

    Add files via upload

---
+++ Simple Calculator/app.js
@@ -0,0 +1,30 @@
+function calculator() {
+  let input;
+  let input2;
+  let input3;
+
+  function init(selector1, selector2, selector3) {
+    input = document.querySelector(selector1);
+    input2 = document.querySelector(selector2);
+    input3 = document.querySelector(selector3);
+  }
+
+  function add() {
+    input3.value = Number(input.value) + Number(input2.value);
+  }
+
+  function subtract() {
+    input3.value = Number(input.value) - Number(input2.value);
+  }
+
+  let obj = {
+    init,
+    add,
+    subtract,
+  };
+
+  return obj;
+}
+
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
